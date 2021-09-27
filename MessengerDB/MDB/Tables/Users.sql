09f49939722457ec88c0d363c57e789808e58d19
Maks Kovalenko (Lenovo) <max.don@live.ru>
2021-09-27 11:58:04 +0300

    Initial commit

---
+++ MessengerDB/MDB/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [MDB].[Users]
+(
+	[Id] uniqueidentifier NOT NULL PRIMARY KEY,
+    [Login] NVARCHAR(128) NOT NULL,
+    [Password] NCHAR(128) NOT NULL,
+    [Name] NVARCHAR(128) NOT NULL,
+)
